4377209796b45f44287865377a72fd1f845176f9
JesperMjornman <47789590+JesperMjornman@users.noreply.github.com>
2020-03-31 16:12:54 +0200

    Update main.c

--- Lab_2/main.c
+++ Lab_2/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include "list.h"
 #include "dictionary.h"
 #include <assert.h>
-
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+void printKeyPair(key_t k, value_t v, int aux);
+bool isEven(key_t k, value_t v, int aux);
 int main(int argc, char *argv[])
 {
 	struct map dict;
@@ -12,10 +17,39 @@ int main(int argc, char *argv[])
 	printf("Added elem with key: %d\n", map_insert(&dict, "12"));
 	
 	assert(list_size(&dict) == 2);
-
 	assert(map_find(&dict, 0) == "10");
 	assert(map_find(&dict, 1) == "12");
 
+	map_remove(&dict, 0);
+
+	assert(list_size(&dict) == 1);
+	assert(map_find(&dict, 0) == NULL);
+	assert(map_find(&dict, 1) == "12");
+
+	printf("Added elem with key: %d\n", map_insert(&dict, "13"));
+	printf("Added elem with key: %d\n", map_insert(&dict, "14"));
+	printf("Added elem with key: %d\n", map_insert(&dict, "16"));
+
+	map_for_each(&dict, &printKeyPair, 0);
+	map_remove_if(&dict, &isEven, 0);
+	printf("After removing all even numbers: \n");
+	map_for_each(&dict, &printKeyPair, 0);
+
+	printf("Release all mem.\n");
+	free_all_mem(&dict);
+	_CrtDumpMemoryLeaks(); // Check for mem - leakage
 	return 0;
 }
 
+void printKeyPair(key_t k, value_t v, int aux)
+{
+	if(!aux)
+		printf("%d : %s\n", k, v);
+	else
+		printf("Key [%d] to Value [%s]\n", k, v);
+}
+
+bool isEven(key_t k, value_t v, int aux)
+{
+	return(!(atoi(v) % 2));
+}
