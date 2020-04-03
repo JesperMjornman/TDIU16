5b8d669a10be4954b5dd18acf1d0b69255c15e53
Jesper Mjörnman <jesmj855@student.liu.se>
2020-04-03 14:43:46 +0200

    Formatting

--- Lab_2/map.h
+++ Lab_2/map.h
@@ -28,12 +28,12 @@ value_t map_find(struct map* m, key_t k);
 value_t map_remove(struct map* m, key_t k);
 
 void map_for_each(struct map* m,
-	void(*exec)(key_t k, value_t v, int aux),
-	int aux);
+									void(*exec)(key_t k, value_t v, int aux),
+									int aux);
 
 void map_remove_if(struct map* m,
-	bool(*cond)(key_t k, value_t v, int aux),
-	int aux);
+									 bool(*cond)(key_t k, value_t v, int aux),
+									 int aux);
 
 size_t free_all_mem(struct map *dict);
 struct association *map_find_associative(struct map *m, key_t k);
