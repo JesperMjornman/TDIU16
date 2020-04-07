728a872db61b21b31f4082b50cf6010ae0a9d69d
JesperMjornman <47789590+JesperMjornman@users.noreply.github.com>
2020-04-07 22:31:48 +0200

    Update map.h

--- src/lib/kernel/map.h
+++ src/lib/kernel/map.h
@@ -1,20 +1,12 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 #include <stdbool.h>
-<<<<<<< HEAD
-#include "list.h"
-
-#define PANIC(message) do { printf("PANIC: %s\n", message); exit(1); } while (0)
-
-typedef char* value_t;
-=======
 #include "lib/kernel/list.h"
 
 /*
  * Remember to type cast to correct value in case of printing.
  */
 typedef void* value_t;
->>>>>>> dev
 typedef int key_t;
 
 struct association
@@ -33,10 +25,7 @@ struct map
 void map_init(struct map* m);
 
 key_t map_insert(struct map* m, value_t v);
-<<<<<<< HEAD
-=======
 bool map_insert_from_key(struct map *m, value_t v, key_t k);
->>>>>>> dev
 value_t map_find(struct map* m, key_t k);
 value_t map_remove(struct map* m, key_t k);
 
@@ -50,9 +39,5 @@ void map_remove_if(struct map* m,
 
 size_t free_all_mem(struct map *dict);
 struct association *map_find_associative(struct map *m, key_t k);
-<<<<<<< HEAD
-struct list_elem *map_remove_from_pointer(struct map *m, struct association *it);
-=======
 struct list_elem *map_remove_from_pointer(struct association *it);
->>>>>>> dev
 #endif
